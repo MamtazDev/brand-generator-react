32466bf4b49c4352a108d7ea69164d85d2d93769
nipaaa <akternipa941@gmail.com>
2023-09-07 10:20:16 +0600

    initial setup

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "./Banner";
+import Unlease from "./Unlease";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <Unlease />
+    </div>
+  );
+};
+
+export default Home;
