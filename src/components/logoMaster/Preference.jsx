80cf8cdba015d45a9d753d04642b9e1201ca3cb2
nipaaa <akternipa941@gmail.com>
2023-09-14 10:49:54 +0600

    update

--- src/components/logoMaster/Preference.jsx
+++ src/components/logoMaster/Preference.jsx
@@ -9,24 +9,27 @@ const Preference = ({ values, handleDislike, handleLike }) => {
         <h2 className="text-dark">Give us your preferences</h2>
         <div className="row gy-3">
           {values?.map((value, index) => (
-            <div key={index} className="col-6 position-relative">
+            <div
+              key={index}
+              className="col-12 col-md-6 col-lg-4 position-relative"
+            >
               <img
                 className="preferenceImg img-fluid"
                 src={geberatelogo}
                 alt="Generate Logo preference"
               />
-              <div className="position-absolute  top-50 start-50 translate-middle d-flex gap-3 fs-3 ">
+              <div className="position-absolute  bottom-0 start-50 translate-middle d-flex gap-5 fs-3 ">
                 <i
                   onClick={() => handleLike(index)}
                   className={`${
-                    value === 1 ? "text-black" : "text-secondary"
-                  } fa-solid fa-thumbs-up`}
+                    value === 1 ? "text-danger" : "text-secondary"
+                  } fa-solid fa-heart bg-white p-2 rounded-circle fs-6`}
                 ></i>
                 <i
                   onClick={() => handleDislike(index)}
                   className={`${
-                    value === -1 ? "text-black" : "text-secondary"
-                  } fa-solid fa-thumbs-down`}
+                    value === -1 ? "text-danger" : "text-secondary"
+                  } fa-solid fa-heart-crack bg-white p-2 rounded-circle fs-6`}
                 ></i>
               </div>
             </div>
